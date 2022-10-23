5f558053e459c44cfb7191dbcea84f7cf9fef2c6
Sudhanshu Vivek <91692978+Sudhanshu61@users.noreply.github.com>
2022-10-23 20:32:06 +0530

    Add files via upload

---
+++ noti.py
@@ -0,0 +1,12 @@
+import time
+from plyer import notification
+
+if __name__ =="__main__":
+   
+        notification.notify(
+        title = "Please drink water" ,
+        message = "About 15.5 cups (3.7 liters) of fluids a day for men. About 11.5 cups (2.7 liters) of fluids a day for women." ,
+        app_icon = r"C:\Users\dell\Desktop\python\icon.ico", 
+        timeout=10
+    )
+  
